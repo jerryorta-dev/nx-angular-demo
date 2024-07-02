853d2a85ecf49bc6de6a87ab016cfd6204ccebde
jerryorta-dev <jerryorta.dev@gmail.com>
2024-07-02 18:18:59 -0500

    feat(api): cors

--- apps/task-manager/api/src/main.ts
+++ apps/task-manager/api/src/main.ts
@@ -10,6 +10,13 @@ import {AppModule} from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
