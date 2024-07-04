e2963e3acf6557564849b38ae99e14e7cf6eb61f
jerryorta-dev <jerryorta.dev@gmail.com>
2024-07-04 06:25:22 -0500

    feat(app): route redirects

--- apps/task-manager/todo/src/app/app.routes.ts
+++ apps/task-manager/todo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
