0fd46364820454654cdadd8cf405a4a9612a7c04
jerryorta-dev <jerryorta.dev@gmail.com>
2024-07-08 05:55:46 -0500

    refactor(storybook): remove image in title

--- apps/storybook-app/.storybook/theme.ts
+++ apps/storybook-app/.storybook/theme.ts
@@ -4,6 +4,6 @@ export default create({
   base: 'dark',
   brandTitle: 'Nx Angular Storybook',
   brandUrl: 'https://nx.dev/recipes/storybook/overview-angular',
-  brandImage: `https://gigasoftware.io/assets/front-end-tech/Nx_FFF.svg`,
+  // brandImage: `https://gigasoftware.io/assets/front-end-tech/Nx_FFF.svg`,
   brandTarget: '_self'
 });
